968e07cecb8134148d4f9d7a425482c4b7820aca
Alex Radevich <virtuain98@gmail.com>
2017-05-01 15:31:17 +0300

    Reading and writing processes are separated

--- Client/main.c
+++ Client/main.c
@@ -1,28 +1,29 @@
-#include"stdio.h"
-#include"stdlib.h"
-#include"sys/types.h"
-#include"sys/socket.h"
-#include"string.h"
-#include"netinet/in.h"
-#include"netdb.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <errno.h>
+#include <unistd.h>
 
-#define BUFF_SIZE 1000
+
+#define BUFF_SIZE 1024
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in addr, cl_addr;
-    int sockfd, retCode;
+    struct sockaddr_in addr;
+    int sockfd, retCode, pid;
     char buffer[BUFF_SIZE];
-    struct hostent * server;
-    char *serverAddr;
+    struct hostent *server;
 
     if (argc < 3) {
         printf("usage: client < ip address >\n");
         exit(1);
     }
 
-    serverAddr = argv[1];
-
+    server = gethostbyname(argv[1]);
+    //server = gethostbyname("127.0.0.1");
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
@@ -34,8 +35,9 @@ int main(int argc, char *argv[])
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(serverAddr);
-    addr.sin_port = PORT;
+    bcopy((char*)server->h_addr_list[0], (char*)&addr.sin_addr.s_addr, server->h_length);
+    addr.sin_port = htons((short)atoi(argv[2]));
+    //addr.sin_port = htons(57057);
 
     retCode = connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));
 
@@ -46,27 +48,45 @@ int main(int argc, char *argv[])
     else
         printf("Connected to the server...\n");
 
-    memset(buffer, 0, BUFF_SIZE);
-    printf("Enter your message(s): ");
+    if ((pid = fork()) < 0) {
+        printf("Error creating process: %s\n", strerror(errno));
+    }
+    else if (pid > 0)
+    {
+        while (1)
+        {
+            memset(buffer, 0, BUFF_SIZE);
+            printf("Enter your message(s): ");
+            fgets(buffer, BUFF_SIZE, stdin);
+            retCode = sendto(sockfd, buffer, BUFF_SIZE, MSG_CONFIRM, (struct sockaddr *) &addr, sizeof(addr));
+            printf("RET_w: %d\n", retCode);
+            sleep(2);
 
-    while (fgets(buffer, BUFF_SIZE, stdin) != NULL) {
-        retCode = sendto(sockfd, buffer, BUFF_SIZE, 0, (struct sockaddr *) &addr, sizeof(addr));
-
-        if (retCode < 0) {
-            printf("Error sending data!\n\t-%s", buffer);
+            if (retCode <= 0) {
+                printf("Error sending data!\n\t-%s", buffer);
+                break;
+            }
+            else
+                printf("sended\n");
         }
-
-        retCode = recvfrom(sockfd, buffer, BUFF_SIZE, 0, NULL, NULL);
-
-        if (retCode < 0) {
-            printf("Error receiving data!\n");
-        }
-        else {
-            printf("Received: ");
-            fputs(buffer, stdout);
-            printf("\n");
-        }
- }
-
- return 0;
+    }
+    else
+    {
+        while (1)
+        {
+            memset(buffer, 0, BUFF_SIZE);
+                  
+            retCode = recvfrom(sockfd, buffer, BUFF_SIZE, 0, NULL, NULL);
+            printf("RET_r: %d\n", retCode);
+            sleep(2);   
+            if (retCode <= 0) {
+                printf("Error receiving data!\n");
+                break;
+            }
+            else {
+                printf("\nReceived: %s\n", buffer);
+            } 
+        }  
+    }
+    return 0;
 }
